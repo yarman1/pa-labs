4246d4565601d80a958b5ae24505c471249f274c
yarman1 <gordienko045@gmail.com>
2022-12-04 13:13:23 +0200

    Add ids algorithm

---
+++ lab-2/algorithms/ids.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const fs = require('fs');
+
+const { drawWay, showMaze } = require('../helpers/serialization');
+
+const MARKERS = {
+  cutoff: 1,
+  failure: 2,
+  success: 3,
+  deadEnd: 4,
+};
+
+const recursiveDLS = function(node, mazeData, limit, analyzeData) {
+  const [ curRowNum, curColNum ] = node.coords;
+  const [ solRowNum, solColNum ] = mazeData.cellEnd;
+
+  analyzeData.allStates += 1;
+
+  if (curRowNum === solRowNum && curColNum === solColNum) {
+    return {
+      marker: MARKERS.success,
+    }
+  } else if (node.depth === limit) {
+    return {
+      marker: MARKERS.cutoff,
+    }
+  }
+
+  if (node.children) {
+    analyzeData.statesInMemory += node.children.length;
+    for (const child of node.children) {
+      drawWay(mazeData, node, child);
+
+      const [ chRowNum, chColNum ] = child.coords;
+      const childCell = mazeData.grid[chRowNum][chColNum];
+      const childResult = recursiveDLS(childCell, mazeData, limit, analyzeData);
+
+      if (childResult.marker === MARKERS.success) {
+        return {
+          marker: MARKERS.success,
+        }
+      }
+
+      if (childResult.marker === MARKERS.cutoff) {
+        return {
+          marker: MARKERS.cutoff,
+        }
+      }
+    }
+
+    return {
+      marker: MARKERS.failure,
+    }
+  }
+
+  analyzeData.deadEnds += 1;
+  return {
+    marker: MARKERS.failure,
+  }
+  
+};
+
+const dls = function(mazeData, limit, analyzeData) {
+  const startCoords = mazeData.cellStart;
+  const [ startRow, startCol ] = startCoords;
+  const startNode = mazeData.grid[startRow][startCol];
+
+  return recursiveDLS(startNode, mazeData, limit, analyzeData);
+};
+
+const ids = function(mazeData) {
+  const analyzeData = {
+    startStates: mazeData.nodesCount,
+    iterations: 0,
+    deadEnds: 0,
+    allStates: 0,
+    statesInMemory: 1,
+  };
+
+  let depth = 0;
+  let result = null;
+  do {
+    result = dls(mazeData, depth, analyzeData);
+    depth += 1;
+    analyzeData.iterations += 1;
+  } while (result.marker != MARKERS.success && result.marker != MARKERS.failure)
+
+  showMaze(mazeData);
+  console.table(analyzeData);
+
+  return {
+    result,
+    analyzeData,
+  };
+};
+
+module.exports = {
+  ids,
+};
+
+// const mazeArrayJSON = fs.readFileSync('maze-data.json');
+// const mazeArray = JSON.parse(mazeArrayJSON);
+
+
+// for (let i = 0; i < 20; i++) {
+//   console.log(i + 1);
+//   const maze = mazeArray[i];
+
+//   setTimeout(()=>{}, 5000);
+
+//   ids(maze);
+// }
