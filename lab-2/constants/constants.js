47dd690ee632127d27a53f3c04b37b10b5fe98e4
yarman1 <gordienko045@gmail.com>
2022-12-04 13:10:44 +0200

    Add constants

---
+++ lab-2/constants/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const COLORS = {
+  path: '\x1b[42m',
+  walls: '\x1b[31m',
+  start: '\x1b[35m',
+  end: '\x1b[46m',
+  default: '\x1b[0m',
+  way: '\x1b[45m',
+};
+
+module.exports = {
+  COLORS,
+};
